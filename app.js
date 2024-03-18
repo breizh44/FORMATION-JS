5bc79c4d54064f4f7ea9297a25a077bc10f845e6
Anthony HAREL <a.harel@polysoude.com>
2024-03-18 11:35:55 +0100

    Etat à fin présentation manipulation du DOM et avant exercice

--- app.js
+++ app.js
@@ -8,4 +8,49 @@ const lis2 = document.querySelectorAll('ul li:first-child')
 console.log(Array.from(lis2))
 
 const ul = document.querySelector('ul')
-console.log(ul.nodeName)
+console.log(ul.nodeName, ul.innerHTML)
+const lis1 = document.querySelector('ul li:first-child')
+console.log(lis1.getAttribute('class'))
+
+console.log(lis1.classList)
+
+/**
+ * Pour faire clignoter en rouge le premier li
+ */
+setInterval(() => {
+    lis1.classList.toggle('red')
+}, 1000)
+
+lis1.style.fontWeight = 'bold'
+lis1.style.color = 'blue'
+
+console.log(getComputedStyle(lis1).color)
+
+/**
+ * Ajout d'un élément au DOM
+ */
+const newLi = document.createElement('li')
+newLi.innerHTML = 'Bonjour les gens'
+//document.querySelector('ul').appendChild(newLi)
+document.querySelector('ul').append(newLi)
+
+const div = document.createElement('div')
+div.innerHTML = 'Bonjour les gens'
+//ul.insertAdjacentElement('beforebegin', div)
+
+
+/**
+ * Parcours des enfants dans le DOM
+ */
+console.log(ul.children)
+console.log(ul.childNodes)
+
+const firstLi = document.querySelector('ul li')
+console.log(
+    firstLi.parentElement
+)
+console.log(
+    firstLi.nextElementSibling
+)
+//firstLi.remove()
+ul.append(firstLi.cloneNode(true))
