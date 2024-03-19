7c298d74836f1cc70bf2bda904b80345bf18599e
Anthony HAREL <a.harel@polysoude.com>
2024-03-19 08:35:05 +0100

    Etat page evenements avec input checkbox avant exercice

--- Events/app.js
+++ Events/app.js
@@ -1,20 +1,47 @@
-/**
- * Gestion de l'evenement de click sur les boutons
- * @param {PointerEvent} event 
- */
-function onButtonClick(event) {
-    console.log('Button click')
-    event.preventDefault() //permet d'empêcher le comportement par défaut de l'élément
-                        //si l'evt est déclenché par un lien, il ne nous renverra pas sur la cible du lien
-    event.stopPropagation()
-    console.log(this)
-    console.log(event.currentTarget)
-}
-
-document.querySelectorAll('button, a').forEach(button => {
-    button.addEventListener('click', onButtonClick)
+document.querySelector('form').addEventListener('submit', (e) => {
+   console.log(e) 
+   //e.preventDefault()
+   const form = e.currentTarget
+   const data = new FormData(form)
+   const firstname = data.get('firstname')
+   if (firstname.length < 2) {
+    console.log('invalide')
+    e.preventDefault() //on empêche le formulaire d'être soumis tant que les données ne sont pas valides
+   }
 })
 
-document.querySelector('div').addEventListener('click', () => {
-    console.log('click div')
-})
+//exemple d'evt qui détecte que le champ a changé
+document.querySelector('input').addEventListener('change', (e) => {
+    console.log('change')
+ })
+
+ document.querySelector('input').addEventListener('input', (e) => {
+    console.log('input', e.currentTarget.value)
+ })
+
+ //si CRT+K pressé sur la page
+ document.addEventListener('keydown', (e) => {
+    if (e.ctrlKey === true && e.key === 'k')
+    {
+        e.preventDefault()
+        console.log('raccourci')
+    }
+ })
+
+ document.querySelector('input').addEventListener('focus', (e) => {
+    console.log(e)
+ })
+
+ const check = document.querySelector('input[type="checkbox"]')
+ console.log(check)
+ document.querySelector('#chk').addEventListener('change', (e) => {
+    console.log(e.currentTarget.checked)
+ }) 
+
+document.querySelector('select')
+ .addEventListener('change', (e) => {
+    console.log(
+        Array.from(e.currentTarget.selectedOptions)
+            .map(option => option.value)
+    )
+ })
