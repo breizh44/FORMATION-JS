59234e56977bee657598f858d7af3e4a4b21bc08
Anthony HAREL <a.harel@polysoude.com>
2024-03-21 13:54:49 +0100

    Notion de Dataset

--- FonctionsUsuellesDuDOM/app.js
+++ FonctionsUsuellesDuDOM/app.js
@@ -1,10 +1,28 @@
-const h1 = document.querySelector('h1')
-console.log('Position par rapport au haut', window.scrollY + h1.getBoundingClientRect().y,
-recursiveOffsetTop(h1))
+// const h1 = document.querySelector('h1')
+// console.log('Position par rapport au haut', window.scrollY + h1.getBoundingClientRect().y,
+// recursiveOffsetTop(h1))
 
-document.querySelector('div').addEventListener('mousemove', e => {
-    console.log(e)
-})
+// document.querySelector('div').addEventListener('mousemove', e => {
+//     console.log(e)
+// })
+
+const div =document.querySelector('div')
+console.log(div.dataset)
+console.log(div.dataset.user)
+div.dataset.hello = "bonjour"
+console.log(div.dataset)
+
+const button = document.querySelector('button')
+let i = 0
+const listener = () => {
+    i++
+    console.log(button.dataset.name)
+    if (i >= 3) {
+        button.removeEventListener('click', listener)
+    }
+}
+button.addEventListener('click', listener)
+
 
 /**
  * 
