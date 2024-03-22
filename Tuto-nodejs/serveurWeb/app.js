d3d1d07895c40bc78291bb7a6d01c997bb138ece
Anthony HAREL <a.harel@polysoude.com>
2024-03-22 15:07:04 +0100

    envoi requete avec paramètres + utilisation insomnia pour tester

--- Tuto-nodejs/serveurWeb/app.js
+++ Tuto-nodejs/serveurWeb/app.js
@@ -2,22 +2,35 @@ import { createReadStream } from 'node:fs'
 import {createServer} from 'node:http'
 import {fileURLToPath} from 'node:url'
 import {dirname, join} from 'node:path'
+import {text} from 'stream/consumers'
 
 const dir = dirname(fileURLToPath(import.meta.url))
 const filename = join(dir, 'index.html')
 
-const server = createServer((req, res) => {
-    console.log(req.url)
-    console.log(req.headers.accept)
-    const file = createReadStream(filename)
-    //file.pipe(res) //par défaut la réponse est fermée quand plus de données sur le pipe
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
+const server = createServer(async (req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`)
+    let body = ''
+    req.on('data', (chunk) => {
+        body += chunk
     })
-    file.pipe(res, {end:false}) //on décide de fermer manuellement le flux de réponse
-    file.on('end', () => {
+    req.on('close', () => {
+        console.log(body)
         res.end()
-    })    
+    })
+
+    // console.log('URL= ', url)    
+    // res.write(`Bonjour ${url.searchParams.get('name')}`) //fonctionne si l'url est de la forme http://localhost:8888/?name=john
+    // res.end()
+    // console.log(req.headers.accept)
+    // const file = createReadStream(filename)
+    // //file.pipe(res) //par défaut la réponse est fermée quand plus de données sur le pipe
+    // res.writeHead(200, {
+    //     'Content-Type': 'text/html'
+    // })
+    // file.pipe(res, {end:false}) //on décide de fermer manuellement le flux de réponse
+    // file.on('end', () => {
+    //     res.end()
+    // })    
 
     //res.write('hello') //pour info res dérive de stream
     //res.end() //clôture la réponse renvoyée au navigateur
