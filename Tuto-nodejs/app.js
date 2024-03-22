a56cd71a1fabc5cea61bab27d0a0b20986b390ae
Anthony HAREL <a.harel@polysoude.com>
2024-03-22 10:55:43 +0100

    Lecture/ecriture de fichiers

--- Tuto-nodejs/app.js
+++ Tuto-nodejs/app.js
@@ -1,5 +1,11 @@
 import fs from 'node:fs'
 import {readFile} from 'node:fs/promises'
+import {writeFile, stat} from 'node:fs/promises'
+import {open} from 'node:fs/promises'
+import {watch} from 'node:fs/promises'
+import {fileURLToPath} from 'node:url'
+import {dirname} from 'node:path'
+import {readdir} from 'node:fs/promises'
 
 //lecture d'un fichier de manière synchrone (donc bloquant)
 //const content = fs.readFileSync('demo.txt', {encoding: 'utf-8'})
@@ -22,3 +28,60 @@ const content3 = await Promise.all([
     await readFile('app.js', {encoding: 'utf-8'})
 ])
 console.log(content3)
+
+await writeFile('demo.txt', 'Bonjour les gens', {encoding:'utf-8', flag: 'a'})
+
+const i = await stat('demo.txt')
+console.log(i)
+
+const file = await open('demo.txt', 'a')
+file.write('hello')
+file.close()
+
+//surveillance d'un répertoire
+// const watcher = watch('./')
+
+// for await (const event of watcher) {
+//     console.log(event)
+// }
+
+//lecture du contenu d'un répertoire puis affichage
+//const wait = (duration) => new Promise(resolve => setTimeout(resolve, duration))
+
+const dir = dirname(fileURLToPath(import.meta.url))
+const files = await readdir(dir, {withFileTypes: true})
+for (const file of files) {
+    const isDir = file.isDirectory()
+    const parts = [
+        isDir ? 'D' : 'F', 
+        file.name
+    ]
+    if (!isDir) {
+        //pb la ligne ci-dessous est bloquante et peut ralentir la boucle
+        const {size} = await stat(file.name)
+        parts.push(`${size}o`)
+    }
+    
+    console.log(parts.join(' - '))
+}
+
+
+//version totalement asynchrone : 
+console.time('code')
+await Promise.allSettled(
+    files.map(async (file) => {
+        const isDir = file.isDirectory()
+        const parts = [
+            isDir ? 'D' : 'F', 
+            file.name
+        ]
+        if (!isDir) {
+            //pb la ligne ci-dessous est bloquante et peut ralentir la boucle
+            const {size} = await stat(file.name)
+            parts.push(`${size}o`)
+        }
+        
+        console.log(parts.join(' - '))
+    })
+)
+console.timeEnd('code')
