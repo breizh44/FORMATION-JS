d75d952435602420128aec20038a15fd717eb0a2
Anthony HAREL <a.harel@polysoude.com>
2024-03-22 16:17:57 +0100

    watcher après améliorations

--- Tuto-nodejs/watcher/watcher.js
+++ Tuto-nodejs/watcher/watcher.js
@@ -1,16 +1,26 @@
 import {exec, spawn} from 'node:child_process'
-import { error } from 'node:console'
+import { watch } from 'node:fs/promises'
 
 const [node, _, file] = process.argv
-const pr = spawn('dir', [], {shell: true})
-pr.stdout.on('data', (data) => {
-    console.log(data.toString('utf-8'))
-})
 
-pr.stderr.on('data', (data) => {
-    console.error(data.toString('utf-8'))
-})
+function spawnNode() {
+    const pr = spawn(node, [file])
+    pr.stdout.pipe(process.stdout)
+    pr.stderr.pipe(process.stderr)
 
-pr.on('close', (code) => {
-    console.log(`process exited with code : ${code}`)
-})
+    pr.on('close', (code) => {
+        if (code !== null) {
+            process.exit(code)
+        }
+    })
+    return pr
+}
+
+let childNodeProces = spawnNode() //on lance le fichier passé en paramètre avec node.js
+const watcher = watch('./', {recursive: true}) //on scrute le répertoire courant et ses enfants de manière récursive
+for await (const event of watcher) {
+    if (event.filename.endsWith('.js')) {
+        childNodeProces.kill('SIGKILL') //on force la fermeture du process Node.js
+        childNodeProces = spawnNode() //dès qu'un fichier javascript du dossier est modifié on relance node.js
+    }
+}
