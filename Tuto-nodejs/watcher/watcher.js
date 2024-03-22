9a5b786a791b5aa61946086a80e00545f1d1a548
Anthony HAREL <a.harel@polysoude.com>
2024-03-22 15:49:21 +0100

    lancement de process avec spawn

--- Tuto-nodejs/watcher/watcher.js
+++ Tuto-nodejs/watcher/watcher.js
@@ -2,11 +2,15 @@ import {exec, spawn} from 'node:child_process'
 import { error } from 'node:console'
 
 const [node, _, file] = process.argv
-
-exec('dir', (error, out, err) => {
-    console.log({
-        error,
-        out, err
-    })
+const pr = spawn('dir', [], {shell: true})
+pr.stdout.on('data', (data) => {
+    console.log(data.toString('utf-8'))
 })
-console.log(process.argv)
+
+pr.stderr.on('data', (data) => {
+    console.error(data.toString('utf-8'))
+})
+
+pr.on('close', (code) => {
+    console.log(`process exited with code : ${code}`)
+})
