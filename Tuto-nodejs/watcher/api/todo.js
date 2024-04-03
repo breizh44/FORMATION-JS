05805eb5318d809d3152efba423d1bba88062aef
Anthony HAREL <a.harel@polysoude.com>
2024-04-03 13:55:06 +0200

    Finalisation de l'api REST CRUD (Create, Read, Update, Delete)

--- Tuto-nodejs/watcher/api/todo.js
+++ Tuto-nodejs/watcher/api/todo.js
@@ -1,12 +1,21 @@
 import { json } from "node:stream/consumers"
-import { createTodo, findTodos } from "../functions/todos_storage.js"
+import { createTodo, findTodos, removeTodo, updateTodo } from "../functions/todos_storage.js"
 
 export async function index(req, res) {
-    const todos = await findTodos()
-    return todos   
+    return findTodos()  
 }
 
 export async function create(req, res) {
-    const todo = await createTodo(await json(req))
-    return todo
+    return createTodo(await json(req))
+}
+
+export async function remove(req, res, url) {
+    const id = parseInt(url.searchParams.get('id'), 10)
+    await removeTodo(id)
+    res.writeHead(204) //statut = 204 => statut no content (requete sans contenu)
+}
+
+export async function update(req, res, url) {
+    const id = parseInt(url.searchParams.get('id'), 10)
+    return updateTodo(id, await json(req))
 }
--- Tuto-nodejs/watcher/functions/todos_storage.js
+++ Tuto-nodejs/watcher/functions/todos_storage.js
@@ -1,4 +1,5 @@
 import { readFile, writeFile } from "node:fs/promises"
+import { NotFoundError } from "./errors.js"
 
 const path = 'storage/todos.json'
 
@@ -26,6 +27,37 @@ export async function findTodos() {
 export async function createTodo({title, completed = false}) {
     const todo = {title, completed, id: Date.now()} //creation du nouveau todo avec les paramètres en entrée
     const todos = [todo, ...await findTodos()] //ajout du nouveau todo en début de liste des todos
-    await writeFile(path, JSON.stringify(todos))
+    await writeFile(path, JSON.stringify(todos, null, 2))
     return todo
+}
+
+/**
+ * @param {number} id
+ * @param {{completed?: boolean, title?: string}} partialTodo
+ * @return {Promise<Todo>}
+ */
+export async function updateTodo(id, partialTodo) {
+    const todos = await findTodos()
+    const todo = todos.find(todo => todo.id === id)
+    if (todo === undefined) {
+        throw new NotFoundError()
+    }
+    Object.assign(todo, partialTodo)
+    await writeFile(path, JSON.stringify(todos, null, 2))
+    return todo
+}
+
+/**
+ * 
+ * @param {number} id
+ * @return {Promise}
+ */
+export async function removeTodo(id) {
+    const todos = await findTodos() 
+    const todo = todos.findIndex(todo => todo.id === id)
+    if (todo === -1) {
+        throw new NotFoundError()
+    }
+    //todos.filter(todo => todo.id !== id) //on garde tous les todos qui ne correspondent pas à celui qu'on veut supprimer
+    await writeFile(path, JSON.stringify(todos.filter(todo => todo.id !== id), null, 2))
 }
