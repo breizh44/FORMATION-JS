0cab465ae1bbd4674768f376586d27e4797da251
Anthony HAREL <a.harel@polysoude.com>
2024-04-04 11:57:13 +0200

    Alias et génériques

--- Tuto-TypeScript/dist/app.js
+++ Tuto-TypeScript/dist/app.js
@@ -1,9 +1,23 @@
 "use strict";
-/* function printId(id: Number | string): void {
-    console.log(id.toString());
-} */
+const user2 = {
+    firstname: 'John',
+    lastname: 'Doe',
+    age: 32
+};
+const user = { firstname: "John", lastname: "Doe" };
 const compteur = document.querySelector('#compteur');
 let i = 0;
+/**
+ * Fonction qui utilise un type générique qui doit étendre un type
+ * qui doit posséder une clé "length" de type number
+ * @param arg
+ * @returns
+ */
+function consoleSize(arg) {
+    console.log(arg.length);
+    return arg;
+}
+const abb = consoleSize(['3', 2]);
 const increment = (e) => {
     e.preventDefault();
     i++;
@@ -13,3 +27,29 @@ const increment = (e) => {
     }
 };
 compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener('click', increment);
+function printId(id) {
+    if (typeof id === "number") {
+        console.log((id * 3).toString());
+    }
+    else {
+        console.log(id.toUpperCase());
+    }
+}
+function isDate(a) {
+    return a instanceof Date;
+}
+/**
+ *
+ * @param arg argument qui peut être de n'importe quel type
+ * @returns retourne un type indéterminé
+ */
+function identity(arg) {
+    return arg;
+}
+const aa = identity(3);
+const aa2 = identity(3);
+function first(arg) {
+    return arg[0];
+}
+const bb = first(["aze", "bze", "cze"]);
+const cc = ["aze", "cze", 3];
--- Tuto-TypeScript/src/app.ts
+++ Tuto-TypeScript/src/app.ts
@@ -1,11 +1,33 @@
-/* function printId(id: Number | string): void {
-    console.log(id.toString());
-} */
+type User = {firstname: string, lastname: string} //alias de type
+type Id = string | number //Autre alias de type
+type Identity<ArgType> = (arg: ArgType) => ArgType
+type Username = User['firstname'] //le type Username ici sera string car firstname est de type string dans le type User
+type Usernamekey = keyof User
 
+const user2 = {
+    firstname: 'John',
+    lastname: 'Doe',
+    age: 32
+}
 
+type UserT = typeof user2 //pour créer un type à la volée à partir de la definition d'un objet
 
-const compteur = document.querySelector('#compteur')
+const user : User = {firstname: "John", lastname: "Doe"}
+const compteur = document.querySelector<HTMLButtonElement>('#compteur')
 let i = 0;
+
+/**
+ * Fonction qui utilise un type générique qui doit étendre un type 
+ * qui doit posséder une clé "length" de type number
+ * @param arg 
+ * @returns 
+ */
+function consoleSize<Type extends {length: number}>(arg: Type): Type {
+    console.log(arg.length)
+    return arg
+}
+
+const abb = consoleSize(['3', 2])
 
 const increment = (e: Event) => {
     e.preventDefault();
@@ -18,3 +40,33 @@ const increment = (e: Event) => {
 
 compteur?.addEventListener('click', increment) 
 
+function printId(id: string | number): void {
+    if (typeof id === "number") {
+        console.log((id * 3).toString())
+    } else {
+        console.log(id.toUpperCase())
+    }
+}
+
+function isDate(a: any): a is Date {
+    return a instanceof Date
+}
+
+/**
+ * 
+ * @param arg argument qui peut être de n'importe quel type
+ * @returns retourne un type indéterminé
+ */
+function identity<ArgType>(arg: ArgType): ArgType {
+    return arg;
+}
+
+const aa = identity<number>(3)
+const aa2 = identity(3)
+
+function first<Type>(arg: Type[]): Type {
+    return arg[0]
+}
+
+const bb = first(["aze", "bze", "cze"])
+const cc: Array<string | number> = ["aze", "cze", 3]
