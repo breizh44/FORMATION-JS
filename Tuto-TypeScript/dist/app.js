a6684a71578e962f78f9a11fcae729136eaa7f79
Anthony HAREL <a.harel@polysoude.com>
2024-04-04 14:13:42 +0200

    Les classe en TypeScript

--- Tuto-TypeScript/dist/app.js
+++ Tuto-TypeScript/dist/app.js
@@ -1,55 +1,101 @@
 "use strict";
-const user2 = {
-    firstname: 'John',
-    lastname: 'Doe',
-    age: 32
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
 };
-const user = { firstname: "John", lastname: "Doe" };
-const compteur = document.querySelector('#compteur');
-let i = 0;
+var _a, _Geometry2_origin;
+function reverse(arr) {
+    //return arr.reverse(); //pb ce code modifie le contenu du tableau ce qu'on ne veut pas
+    return [...arr].reverse(); //on fait un reverse sur une copie du tableau
+}
+class A {
+    constructor() {
+        this.a = 3; //ne peut être accédée qu'à l'interieur de la classe
+        this.b = 4; //idem que private sauf que les enfants (classe dérivée) peuvent y accéder
+        this.c = 5; //accessible de partout (visibilité par défaut)
+    }
+}
+const aInstance = new A();
+class Collection {
+    constructor(items) {
+        this.items = items;
+    }
+    add(item) {
+        this.items.push(item);
+        return this;
+    }
+    first() {
+        return this.items[0] || null;
+    }
+    isEqual(a) {
+        return a.items === this.items;
+    }
+}
+class subCollection extends Collection {
+    constructor() {
+        super(...arguments);
+        this.a = 3;
+    }
+}
+const maCollec = new Collection([1, 2]);
+const maCollec2 = new Collection([1, 2]); //fonctionne également
+const bFirst = maCollec2.first();
+const maSubColl = new subCollection([2, 2]);
+//maSubColl.isEqual(maCollec) //ne compile pas car subCollection possède un attribut a en plus par rapport à la classe Collection
+class Point {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+    }
+}
+class Geometry {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.surface = 0;
+    }
+}
+function getX(p) {
+    return p.x;
+}
+getX(new Geometry()); //fonctionne car la classe Geometry recouvre la classe Point en terme d'attributs
+//Duck Typing : "si ça fait coincoin et que ça marche comme un canard, c'est un canard" ;-))
 /**
- * Fonction qui utilise un type générique qui doit étendre un type
- * qui doit posséder une clé "length" de type number
- * @param arg
- * @returns
+ * Classe abstraite
  */
-function consoleSize(arg) {
-    console.log(arg.length);
-    return arg;
-}
-const abb = consoleSize(['3', 2]);
-const increment = (e) => {
-    e.preventDefault();
-    i++;
-    const span = compteur === null || compteur === void 0 ? void 0 : compteur.querySelector('span');
-    if (span) {
-        span.innerText = i.toString();
-    }
-};
-compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener('click', increment);
-function printId(id) {
-    if (typeof id === "number") {
-        console.log((id * 3).toString());
-    }
-    else {
-        console.log(id.toUpperCase());
+class AbstractGeometry {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
     }
 }
-function isDate(a) {
-    return a instanceof Date;
+class Triangle extends AbstractGeometry {
+    constructor() {
+        super(...arguments);
+        this.x = 2;
+        this.y = 2;
+    }
+    surface() {
+        return 3;
+    }
 }
-/**
- *
- * @param arg argument qui peut être de n'importe quel type
- * @returns retourne un type indéterminé
- */
-function identity(arg) {
-    return arg;
+class Geometry2 {
+    constructor(x, y) {
+    }
 }
-const aa = identity(3);
-const aa2 = identity(3);
-function first(arg) {
-    return arg[0];
+_a = Geometry2;
+_Geometry2_origin = { value: void 0 };
+(() => {
+    __classPrivateFieldSet(_a, _a, { x: 0, y: 0 }, "f", _Geometry2_origin);
+})();
+class Triangle2 {
+    constructor(x, y) {
+    }
 }
-const bb = first(["aze", "bze", "cze"]);
-const cc = ["aze", "cze", 3];
+function shapeGenerator(shapeType, x, y) {
+    return new shapeType(x, y).surface();
+}
+//shapeGenerator(Geometry2, 10, 20) //ne compile pas car Geometry2 ne possède pas de dméthode surface
+shapeGenerator(Triangle, 10, 20);
--- Tuto-TypeScript/src/app.ts
+++ Tuto-TypeScript/src/app.ts
@@ -1,72 +1,113 @@
-type User = {firstname: string, lastname: string} //alias de type
-type Id = string | number //Autre alias de type
-type Identity<ArgType> = (arg: ArgType) => ArgType
-type Username = User['firstname'] //le type Username ici sera string car firstname est de type string dans le type User
-type Usernamekey = keyof User
-
-const user2 = {
-    firstname: 'John',
-    lastname: 'Doe',
-    age: 32
+function reverse<T>(arr: readonly T[]): T[] {
+    //return arr.reverse(); //pb ce code modifie le contenu du tableau ce qu'on ne veut pas
+    return [...arr].reverse(); //on fait un reverse sur une copie du tableau
 }
 
-type UserT = typeof user2 //pour créer un type à la volée à partir de la definition d'un objet
-
-const user : User = {firstname: "John", lastname: "Doe"}
-const compteur = document.querySelector<HTMLButtonElement>('#compteur')
-let i = 0;
-
-/**
- * Fonction qui utilise un type générique qui doit étendre un type 
- * qui doit posséder une clé "length" de type number
- * @param arg 
- * @returns 
- */
-function consoleSize<Type extends {length: number}>(arg: Type): Type {
-    console.log(arg.length)
-    return arg
+class A {
+    private a = 3 //ne peut être accédée qu'à l'interieur de la classe
+    protected b = 4 //idem que private sauf que les enfants (classe dérivée) peuvent y accéder
+    public c = 5 //accessible de partout (visibilité par défaut)
 }
 
-const abb = consoleSize(['3', 2])
+const aInstance = new A();
 
-const increment = (e: Event) => {
-    e.preventDefault();
-    i++;
-    const span = compteur?.querySelector('span');
-    if (span) {
-        span.innerText = i.toString()
+class Collection<T> {
+    constructor(private items: T[]) {
+
+    }
+
+    add(item: T): this {
+        this.items.push(item)
+        return this
+    }
+
+    first(): T | null {
+        return this.items[0] || null
+    }
+
+    isEqual(a: this) { //ici on attend en paramètre qq chose qui est de la même forme que la classe courante
+        return a.items === this.items
     }
 }
 
-compteur?.addEventListener('click', increment) 
+class subCollection<T> extends Collection<T> {
+    a = 3
+}
 
-function printId(id: string | number): void {
-    if (typeof id === "number") {
-        console.log((id * 3).toString())
-    } else {
-        console.log(id.toUpperCase())
+const maCollec = new Collection<number>([1, 2])
+const maCollec2 = new Collection([1, 2]) //fonctionne également
+const bFirst = maCollec2.first()
+const maSubColl = new subCollection([2,2])
+//maSubColl.isEqual(maCollec) //ne compile pas car subCollection possède un attribut a en plus par rapport à la classe Collection
+
+
+class Point {
+    x = 0
+    y = 0
+}
+
+class Geometry {
+    x = 0 
+    y = 0
+    surface = 0
+}
+
+function getX(p: Point) {
+    return p.x
+}
+
+getX(new Geometry());  //fonctionne car la classe Geometry recouvre la classe Point en terme d'attributs
+                       //Duck Typing : "si ça fait coincoin et que ça marche comme un canard, c'est un canard" ;-))
+
+
+/**
+ * Classe abstraite
+ */
+abstract class AbstractGeometry {
+    x = 0 
+    y = 0
+    abstract surface() : number //méthode abstraite
+}  
+
+class Triangle extends AbstractGeometry {
+    x = 2
+    y = 2
+
+    surface() {
+        return 3
+    }    
+}
+
+class Geometry2 {
+    static #origin: {x:number, y: number}
+
+    static {
+        Geometry2.#origin = {x:0, y:0}
+    }
+
+    constructor(x: number, y: number) {
+
+    }
+      
+}
+
+class Triangle2 {
+    constructor(x: number, y: number) {
+
+    }    
+}
+
+type InstantiableShape = {
+    //new(x: number, y: number): any
+    new(x: number, y: number): { //le type InstantiableShape possède une méthode new qui prend en entrée les paramètres x et y
+                                //et retourne un objet qui possède une fonction surface qui retourne un nombre
+        surface: () => number
     }
 }
 
-function isDate(a: any): a is Date {
-    return a instanceof Date
+function shapeGenerator(shapeType: InstantiableShape, x: number, y: number) {
+    return new shapeType(x, y).surface()
 }
 
-/**
- * 
- * @param arg argument qui peut être de n'importe quel type
- * @returns retourne un type indéterminé
- */
-function identity<ArgType>(arg: ArgType): ArgType {
-    return arg;
-}
-
-const aa = identity<number>(3)
-const aa2 = identity(3)
-
-function first<Type>(arg: Type[]): Type {
-    return arg[0]
-}
-
-const bb = first(["aze", "bze", "cze"])
-const cc: Array<string | number> = ["aze", "cze", 3]
+//shapeGenerator(Geometry2, 10, 20) //ne compile pas car Geometry2 ne possède pas de dméthode surface
+shapeGenerator(Triangle, 10, 20)
